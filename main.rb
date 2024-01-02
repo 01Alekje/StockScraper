db9357f6804830616051a3c8e250b16087969283
01Alekje <01Alexanderkjellberg@gmail.com>
2024-01-02 20:42:44 +0100

    some more stuff

--- main.rb
+++ main.rb
@@ -1,17 +1,30 @@
 require 'httparty'
 require 'nokogiri'
 
-# Fetching response from Avanza's newspage
-response = HTTParty.get("https://www.placera.se/placera/forstasidan.html")
+@links = {}
 
-# Parse some shit and stuff, yeah
-document = Nokogiri::HTML(response.body)
-
-articles = document.css("div.cq-puff")
-
-document.css('div.cq-puff div').each do |line|
-  keyword = line.css('span.arrowRight')
-  if keyword.text.include?("Köpråd") or keyword.text.include?("Köptips")
-    puts line.css('h1')
-  end
+def fetch_url_body (url)
+	response = HTTParty.get(url)
+	Nokogiri::HTML(response.body)
 end
+
+def get_article_links ()
+	document = fetch_url_body("https://www.placera.se/placera/forstasidan.html")
+
+	document.css('div.cq-puff div').each do |line|
+		keyword = line.css('span.arrowRight')
+		if keyword.text.include?("Köpråd") or keyword.text.include?("Köptips")
+		@links[line.css('h1 a').children.first.text] = 'https://www.placera.se' + line.css('h1 a')[0]['href']
+		end
+	end
+end
+
+# extracts stock recommendations from an article, if possible (table.instrumentSummaryDetails has info)
+def extract_stock_recommendations (url)
+	document = fetch_url_body(url)
+end
+
+#get_article_links()
+
+
+puts @links
