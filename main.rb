ca512444d8203cbf58f202c9a4b67bf4f4d7f6cc
01Alekje <01Alexanderkjellberg@gmail.com>
2024-01-04 02:33:27 +0100

    finds more of relevant articles

--- main.rb
+++ main.rb
@@ -7,14 +7,17 @@ def fetch_url_body (url)
 	Nokogiri::HTML(response.body)
 end
 
-# scans for relevant articles (todo: find more of the relevant articles)
-def get_article_links ()
+# scans for relevant articles
+def get_articles ()
 	@articles = []
 	document = fetch_url_body("https://www.placera.se/placera/forstasidan.html")
 
 	document.css('div.cq-puff div').each do |line|
 		keyword = line.css('span.arrowRight')
-		if keyword.text.include?("Köpråd") or keyword.text.include?("Köptips")
+		if keyword.text.include?("Köp")
+			tmp = Article.new(('https://www.placera.se' + line.css('h1 a')[0]['href']), (line.css('h1 a').children.first.text))
+			@articles << tmp
+		elsif line.css('h1 a').children.first != nil and line.css('h1 a').children.first.text.downcase.include?("köp")
 			tmp = Article.new(('https://www.placera.se' + line.css('h1 a')[0]['href']), (line.css('h1 a').children.first.text))
 			@articles << tmp
 		end
@@ -27,7 +30,7 @@ def extract_stock_recommendations (article)
 	document = fetch_url_body(article.getUrl())
 	document.css('table.instrumentSummaryDetails tbody tr').each do |row|
 		st_name = row.css('a').children.first.text
-		st_advice = row.css('td.tRight.plcPositive').children.first.text
+		st_advice = row.css('td.tRight').children.first.text
 		article.add_stock_recommendation(st_name, st_advice)
 	end
 end
@@ -51,10 +54,12 @@ def find_intersections (articles)
 	intersections
 end
 
-articles = get_article_links()
+articles = get_articles()
 
 articles.each do |article|
 	extract_stock_recommendations(article)
+	p article.getName()
+	p article.get_stock_recommendations()
 end
 
 p find_intersections(articles)
